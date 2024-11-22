6a4138193812ee5a4ef4b68df4ee073c5b10fd8c
Shubham Mishra <95081360+cyberdslayer@users.noreply.github.com>
2024-11-22 09:13:33 +0000

    conversion in js

---
+++ basics/conversions.js
@@ -0,0 +1,20 @@
+// let score = 33;
+// let score = "33";
+// let score = "33abc"
+// let score = null;
+let score = undefined;
+let valueInNumber = Number(score);
+console.log(typeof score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 45;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof(stringNumber));
